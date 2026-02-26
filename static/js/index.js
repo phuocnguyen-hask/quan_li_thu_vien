0cf7d2c402e081a8f21c705146074031a6a38819
cat <phuocnguyn203@gmail.com>
2026-02-26 13:47:07 +0700

    fix index.js

--- static/js/index.js
+++ static/js/index.js
@@ -34,3 +34,13 @@ if (!slider || !prevBtn || !nextBtn) {
     }
   });
 }
+document.querySelectorAll('.click-to-watch').forEach(card => {
+    card.addEventListener('click', () => {
+        const img = card.querySelector('img');
+        if (!img) return;
+
+        const posterSrc = encodeURIComponent(img.getAttribute('src'));
+
+        window.location.href = `watch.html?poster=${posterSrc}`;
+    });
+});
