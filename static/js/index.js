475881cb849e43fef654f0729cf92746b1e58c1f
cat <phuocnguyn203@gmail.com>
2026-02-26 12:48:15 +0700

    add play button

--- static/js/index.js
+++ static/js/index.js
@@ -1,33 +1,36 @@
 const slider = document.querySelector('.movie-slider');
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
+if (!slider || !prevBtn || !nextBtn) {
+  console.error('Slider or navigation buttons not found!');
+}else {
+  const scrollAmount = 300;
 
-const scrollAmount = 500;
+  nextBtn.addEventListener('click', () => {
+    if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth - 5) {
+      slider.scrollTo({
+        left: 0,
+        behavior: 'smooth'
+      });
+    } else {
+      slider.scrollBy({
+        left: scrollAmount,
+        behavior: 'smooth'
+      });
+    }
+  });
 
-nextBtn.addEventListener('click', () => {
-  if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth - 5) {
-    slider.scrollTo({
-      left: 0,
-      behavior: 'smooth'
-    });
-  } else {
-    slider.scrollBy({
-      left: scrollAmount,
-      behavior: 'smooth'
-    });
-  }
-});
-
-prevBtn.addEventListener('click', () => {
-  if (slider.scrollLeft <= 0) {
-    slider.scrollTo({
-      left: slider.scrollWidth,
-      behavior: 'smooth'
-    });
-  } else {
-    slider.scrollBy({
-      left: -scrollAmount,
-      behavior: 'smooth'
-    });
-  }
-});
+  prevBtn.addEventListener('click', () => {
+    if (slider.scrollLeft <= 0) {
+      slider.scrollTo({
+        left: slider.scrollWidth,
+        behavior: 'smooth'
+      });
+    } else {
+      slider.scrollBy({
+        left: -scrollAmount,
+        behavior: 'smooth'
+      });
+    }
+  });
+}
